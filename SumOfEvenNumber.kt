c4c57fd99c61d84496218da730ca96abd44c64b0
bhoomi2716 <bhoomiwadhvana@gmail.com>
2024-11-26 14:21:08 +0530

    commit

---
+++ SumOfEvenNumber.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+//    var i=1
+//    var sum=0
+//    while (i<=100)
+//    {
+//        if(i%2==0)
+//        {
+//            sum+=i
+//        }
+//        i++
+//    }
+//
+//    print("Sum Of All Even Numbers Between 1 to 100 Is : ")
+//    print(sum)
+
+    var name= arrayOf("b","h","o","o","m","i")
+    var length = name.size
+
+    println(length)
+}
